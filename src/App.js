d5faae42579eb545ae623a621a9d5e0f44a15e6a
cristian.mitu <cristian.mitu@mfortune.co.uk>
2022-12-19 14:35:47 +0200

    add modal reducer

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import CartContainer from "./CartContainer";
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { calculateTotal } from "./features/cart/cartSlice";
+import Modal from "./components/Moda";
 
 function App() {
     const { cartItems } = useSelector((store) => store.cart);
+    const { isOpen } = useSelector((store) => store.modal);
     const dispatch = useDispatch();
-    
+
     useEffect(() => {
         dispatch(calculateTotal());
     }, [cartItems]);
 
   return <main>
+    {isOpen && <Modal />}
     <Navbar />
     <CartContainer />
   </main>;
